13ab5522dea7476247183412ee56e58d4231baa2
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-10-17 16:25:29 -0500

    changed var name

--- src/LogicalOpTable.java
+++ src/LogicalOpTable.java
@@ -3,56 +3,56 @@ public class LogicalOpTable {
   public static void main(String[] args) {
 
     byte p, q;
-    byte not;
+    byte notp;
 
     System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
 
     p = q = 1;
 
     if ( p != 1) {
-      not = 1;
+      notp = 1;
     } else {
-      not = 0;
+      notp = 0;
     };
 
     System.out.print(p + "\t" + q + "\t");
     System.out.print((p&q) + "\t" + (p|q) + "\t");
-    System.out.println((p^q) + "\t" + not);
+    System.out.println((p^q) + "\t" + notp);
 
     q = 0;
 
     if ( p != 1) {
-      not = 1;
+      notp = 1;
     } else {
-      not = 0;
+      notp = 0;
     };
 
     System.out.print(p + "\t" + q + "\t");
     System.out.print((p&q) + "\t" + (p|q) + "\t");
-    System.out.println((p^q) + "\t" + not);
+    System.out.println((p^q) + "\t" + notp);
 
     p = 0; q = 1;
 
     if ( p != 1) {
-      not = 1;
+      notp = 1;
     } else {
-      not = 0;
+      notp = 0;
     };
 
     System.out.print(p + "\t" + q + "\t");
     System.out.print((p&q) + "\t" + (p|q) + "\t");
-    System.out.println((p^q) + "\t" + not);
+    System.out.println((p^q) + "\t" + notp);
 
     q = 0;
 
     if ( p != 1) {
-      not = 1;
+      notp = 1;
     } else {
-      not = 0;
+      notp = 0;
     };
 
     System.out.print(p + "\t" + q + "\t");
     System.out.print((p&q) + "\t" + (p|q) + "\t");
-    System.out.println((p^q) + "\t" + not);
+    System.out.println((p^q) + "\t" + notp);
   }
 }
